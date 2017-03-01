8016893855cf529511e843a233cc2e3bdea7bf98
Jack Hughes <jack@jackhughesweb.com>
2017-03-01 10:44:20 +0000

    Added testing for audio manager

--- src/audio/AudioManager.java
+++ src/audio/AudioManager.java
@@ -8,11 +8,11 @@ import javafx.scene.media.MediaPlayer;
 public class AudioManager implements gui.UserSettingsObserver {
 
     public SFXResources sfx = new SFXResources();
-    MediaPlayer musicPlayer;
-    float musicVolume = (float) 100.0;
-    float sfxVolume = (float) 100.0;
+    public MediaPlayer musicPlayer;
+    private float musicVolume = (float) 100.0;
+    private float sfxVolume = (float) 100.0;
 
-    UserSettings s;
+    private UserSettings s;
 
     public AudioManager(UserSettings s, GUIManager m) {
         this.s = s;
@@ -25,7 +25,7 @@ public class AudioManager implements gui.UserSettingsObserver {
         updateVolume();
     }
 
-    public void updateVolume() {
+    private void updateVolume() {
         float musicVol = s.getMusicVolume();
         musicVolume = musicVol / (float) 100.0;
         if (musicPlayer != null) {
@@ -39,12 +39,7 @@ public class AudioManager implements gui.UserSettingsObserver {
     public void startMusic(Media media) {
         musicPlayer = new MediaPlayer(media);
         musicPlayer.setVolume(musicVolume);
-        musicPlayer.setOnEndOfMedia(new Runnable() {
-            @Override
-            public void run() {
-                startMusic(media);
-            }
-        });
+        musicPlayer.setOnEndOfMedia(() -> startMusic(media));
         musicPlayer.play();
     }
 
