4a7e96eb60c9efee46a94a10dbf8241c271a1932
fxg565 <fxg565@cs.bham.ac.uk>
2017-03-02 14:25:22 +0000

    modified the ServerGameStateSender

--- src/integrationServer/ServerGameStateSender.java
+++ src/integrationServer/ServerGameStateSender.java
@@ -1,10 +1,12 @@
 package integrationServer;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
-import networkingClient.ClientSender;
 import networkingServer.ServerMsgSender;
-import physics.InputHandler;
 import players.ServerMinimumPlayer;
 
 /**
@@ -16,8 +18,49 @@ public class ServerGameStateSender {
 
 	private ServerMsgSender toClient;
 	private ArrayList<ServerMinimumPlayer> players;
-	private int id;
+	int frames = 0;
+	/* Dealing with sending the information */
+	private long delayMilliseconds = 33;
 
+	public ServerGameStateSender(ServerMsgSender toClient, ArrayList<ServerMinimumPlayer> players){
+
+	}
+
+	public void startSending(){
+
+		ScheduledExecutorService scheduler =
+			     Executors.newScheduledThreadPool(1);
+		Runnable sender = new Runnable() {
+		       public void run() {
+		    	   frames ++;
+		    	   sendClient();
+
+		       }
+		     };
+
+		ScheduledFuture<?> senderHandler =
+				scheduler.scheduleAtFixedRate(sender, 0, delayMilliseconds, TimeUnit.MILLISECONDS);
+
+		//for testing purposes:
+
+//		Runnable frameCounter = new Runnable() {
+//		       public void run() {
+//		    	   System.out.println("server Sending frames " + frames);
+//		    	   frames = 0;
+//
+//		       }
+//		     };
+//
+//		ScheduledFuture<?> frameCounterHandle =
+//				scheduler.scheduleAtFixedRate(frameCounter, 0, 1, TimeUnit.SECONDS);
+
+	}
+
+	private void sendClient() {
+
+		//TODO
+
+	}
 
 
 
