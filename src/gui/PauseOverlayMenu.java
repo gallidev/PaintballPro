d2d30aeade74e23195334df4574ab68c8d5f15fa
Jack Hughes <jack@jackhughesweb.com>
2017-03-02 13:54:24 +0000

    Changed music to not be static

--- src/gui/PauseOverlayMenu.java
+++ src/gui/PauseOverlayMenu.java
@@ -1,5 +1,29 @@
 package gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.SubScene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 public class PauseOverlayMenu {
-	// TODO: implement the pause screen
+
+    private SubScene getScene(double width, double height) {
+        GridPane p = new GridPane();
+        SubScene s = new SubScene(p, width, height);
+
+        Label title = new Label("Paused");
+        p.add(title, 0, 0);
+
+        p.setAlignment(Pos.CENTER);
+        p.setHgap(10);
+        p.setVgap(10);
+        p.setPadding(new Insets(25, 25, 25, 25));
+
+        p.add(title, 0, 0);
+        p.setStyle("-fx-background-color: rgba(255,255,255,0.4); -fx-pref-height: 100px;");
+
+        return s;
+    }
+
 }
