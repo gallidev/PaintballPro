340f2a0b3d851319801a66f008c9c67b0319ae69
axk580 <axk580@cs.bham.ac.uk>
2017-03-14 23:47:01 +0000

    Flag now contains its possible locations

--- src/rendering/Objective.java
+++ src/rendering/Objective.java
@@ -1,7 +1,17 @@
 package rendering;
 
-class Objective
+public class Objective
 {
 	int x, y;
 	ObjectiveType type;
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
 }
