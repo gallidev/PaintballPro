2a37f44982099dbbe5a545e1d5c2dae2ac97536b
Jack Hughes <jack@jackhughesweb.com>
2017-03-19 10:36:27 +0000

    Compressed audio

--- src/audio/MusicResources.java
+++ src/audio/MusicResources.java
@@ -7,9 +7,9 @@ import java.util.Random;
 
 public class MusicResources {
 	// Track1 from http://www.freesound.org/people/Headphaze/sounds/177101/ (Creative Commons v3.0)
-	public Media track1 = new Media(new File("res/assets/music/track1.wav").toURI().toString());
+	public Media track1 = new Media(new File("res/assets/music/track1.mp3").toURI().toString());
 	// Track2 from http://www.freesound.org/people/dingo1/sounds/243979/ (Creative Commons v3.0)
-	public Media track2 = new Media(new File("res/assets/music/track2.wav").toURI().toString());
+	public Media track2 = new Media(new File("res/assets/music/track2.mp3").toURI().toString());
 
 	public Media allTracks[] = {track1, track2};
 
