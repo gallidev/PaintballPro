b40a2be1ed8abd42e741075e8fe7056e48262c53
fxg565 <fxg565@cs.bham.ac.uk>
2017-03-22 18:57:23 +0000

    fixed the multiplayer bug

--- src/networking/game/UDPClient.java
+++ src/networking/game/UDPClient.java
@@ -308,12 +308,15 @@ public class UDPClient extends Thread {
 			double angle = Double.parseDouble(actions[4]);
 
 			boolean visibility = true;
-			if (actions[5].equals("false"))
+			if (actions[5].equals("false")){
 				visibility = false;
+			}
 
 			boolean eliminated = false;
-			if(actions[5].equals("true"))
+			if(actions[6].equals("true")){
 				eliminated = true;
+			}
+
 
 			if (gameStateReceiver != null) {
 				gameStateReceiver.updatePlayer(id, x, y, angle, visibility, eliminated);
